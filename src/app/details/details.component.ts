86b82957e6f93063b595f673bdc6f58322b294fd
AndrickSilva <silva.andrick2001@gmail.com>
2022-10-06 11:49:12 +0530

    completed add product func

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-
+import { ProdApiService } from "../service/prod-api.service";
 
 @Component({
   selector: 'app-details',
@@ -9,18 +9,24 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient, private api: ProdApiService) { }
 
   itemId!: string | null
   Url: string = 'http://localhost:3000/products/'  //change url
   newUrl!: string
   items!: any
+
+  cartBtn: boolean = false
+  cartBtnText: string = 'Add to cart'
   ngOnInit(): void {
     this.itemId = this.route.snapshot.paramMap.get("id")
-    this.newUrl = `${this.Url}/${this.itemId}`
+    this.newUrl = `${this.Url}${this.itemId}`
 
     this.getProduct().subscribe(res => {
       this.items = res
+      console.log(this.items);
+
+      // this.postDetails(this.items)
     });
   }
 
@@ -34,5 +40,18 @@ export class DetailsComponent implements OnInit {
     return ((100 - percent) * total / 100).toFixed(2)
   }
 
+  // Post user 
+  postDetails() {
+    this.api.postCartItems(this.items).subscribe(resp=>{
+      if(!this.items) return
+      alert("Added Successfully")
+      console.log(resp);
+    })
+    this.cartBtn = true
+    this.cartBtnText = 'Added Successfully'
+  }
 
+  // postDetails(){
+  //   console.log("hello");
+  // }
 }
--- src/app/service/prod-api.service.ts
+++ src/app/service/prod-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,17 +8,36 @@ export class ProdApiService {
 
   constructor(private http: HttpClient) { }
 
+  // get all products
   getAllProducts() {
     return this.http.get("http://localhost:3000/products")
+    .pipe(map((res: any) => {
+      return res;
+    }))
   }
 
+  // GET CART ITEM
   getCartItems() {
     return this.http.get("http://localhost:3000/cart")
+    .pipe(map((res: any)=>{
+      return res
+    }))
   }
 
   //delete cart item
   deleteCartItems(id: any) {
     return this.http.delete("http://localhost:3000/cart/" + id)
+    .pipe(map((res: any)=>{
+      return res
+    }))
+  }
+
+  //delete cart item
+  postCartItems(data: any) {
+    return this.http.post("http://localhost:3000/cart/", data)
+    .pipe(map((res: any)=>{
+      return res
+    }))
   }
 
 }
